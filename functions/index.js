6efb7a6d06f31ac6145984309b7d3cdbb95aa21e
ferehan <ahmedferehan7@gmail.com>
2026-02-15 22:08:50 +0300

    setting up stripe,backend payment API using function and without function

---
+++ functions/index.js
@@ -0,0 +1,41 @@
+
+
+const {setGlobalOptions} = require("firebase-functions");
+const {onRequest} = require("firebase-functions/https");
+const logger = require("firebase-functions/logger");
+const express =require('express')
+const cors=require('cors')
+const dotenv=require('dotenv')
+dotenv.config();
+const stripe=require('stripe')(process.env.SSTRIPE_KEY);
+
+const app=express();
+app.use(express.json());
+app.use(cors({origin:true}))
+
+app.get("/",(req,res)=>{
+    res.status(200).json({
+        message:"success"
+    })
+})
+app.post("/payment/create",async(req,res)=>{
+const total=req.query.total
+
+if(total >0){
+const paymentIntent =await stripe.paymentIntents.create({
+    amount :total,
+currency:"usd"
+})
+res.status(201).json({
+    clientSecret:paymentIntent.client_secret,
+})
+
+}else{
+    res.status(403).json({
+        message:"total payment intent ayiseram"
+    })
+}
+})
+exports.api=onRequest(app)
+setGlobalOptions({ maxInstances: 10 });
+
